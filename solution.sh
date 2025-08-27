3263b2172beb138db1da72d082c9da4242e1d222
jkraagav <jkraagav@gmail.com>
2025-08-27 11:21:12 +0530

    Add files via upload

---
+++ solution.sh
@@ -0,0 +1,38 @@
+
+#!/bin/bash
+
+sort numbers.txt
+
+ipconfig getifaddr en0
+
+cat readme.txt
+
+wc -l data.csv
+
+grep -lri error logs/
+
+tail -10 app.log
+
+chmod +x script.sh
+
+grep -i -r TODO *.py
+
+history 20
+
+ps aux --sort -%mem
+
+find / -type d -name "backup"
+
+sed 's/foo/bar/g' example.txt > new_example.txt
+
+
+
+
+
+
+
+
+
+
+
+       
